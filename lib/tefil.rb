dfb63043fdb460a411068825ccf32e8182cb9cdf
ippei94da <ippei94da@gmail.com>
2012-12-16 10:27:19 +0900

    small revision

--- lib/tefil.rb
+++ lib/tefil.rb
@@ -28,55 +28,55 @@ require "tempfile"
 #
 module TextFilter
 
-	class NotRedefinedMethodError < Exception; end
-	class TypeError < Exception; end
+  class NotRedefinedMethodError < Exception; end
+  class TypeError < Exception; end
 
-	# 保持している入力ファイル対して、順に処理を実行する。
-	# filenames.size が 0 ならば STDIN からの入力を待つことになる。
-	#
-	# STDIN からの入力だった場合、出力先は必ず STDOUT になる。
-	# STDIN からの入力ではない、すなわちファイル入力であった場合、
-	#   - overwrite_flag が false ならば STDOUT に出力する。
-	#   - overwrite_flag が true ならば 個々のファイルに上書きする。
-	#
-	# Process of each file is defined in 'process_stream' method.
-	#
-	def self.run(filenames, overwrite_flag = false)
-		#p self; exit
-		if filenames.size == 0 
-			self.process_stream( $stdin, $stdout )
-		else
-			#p filenames
-			filenames.each do |filename|
-				if overwrite_flag
-					tempfile = Tempfile.new("tefil", "/tmp")
-					File.open(filename, "r") do |input_file|
-						self.process_stream(input_file, tempfile)
-					end
-					tempfile.close
-					tempfile.open
-					File.open(filename, "w") do |output_file|
-						tempfile.each { |line| output_file.puts(line) }
-					end
-				else
-					File.open(filename, "r") do |input_file|
-						self.process_stream(input_file, $stdout)
-					end
-				end
-			end
-		end
-	end
+  # 保持している入力ファイル対して、順に処理を実行する。
+  # filenames.size が 0 ならば STDIN からの入力を待つことになる。
+  #
+  # STDIN からの入力だった場合、出力先は必ず STDOUT になる。
+  # STDIN からの入力ではない、すなわちファイル入力であった場合、
+  #   - overwrite_flag が false ならば STDOUT に出力する。
+  #   - overwrite_flag が true ならば 個々のファイルに上書きする。
+  #
+  # Process of each file is defined in 'process_stream' method.
+  #
+  def self.run(filenames, overwrite_flag = false)
+    #p self; exit
+    if filenames.size == 0 
+      self.process_stream( $stdin, $stdout )
+    else
+      #p filenames
+      filenames.each do |filename|
+        if overwrite_flag
+          tempfile = Tempfile.new("tefil", "/tmp")
+          File.open(filename, "r") do |input_file|
+            self.process_stream(input_file, tempfile)
+          end
+          tempfile.close
+          tempfile.open
+          File.open(filename, "w") do |output_file|
+            tempfile.each { |line| output_file.puts(line) }
+          end
+        else
+          File.open(filename, "r") do |input_file|
+            self.process_stream(input_file, $stdout)
+          end
+        end
+      end
+    end
+  end
 
-	private
+  private
 
-	# Process a file.
-	# An argument 'in_io' indicates an io (file handle) for input.
-	# Another argument 'out_io' indicates an io (file handle) for output.
-	# This method must be redefined in a subclass or be overridden.
-	# If not redefined, raise an exception TextFilter::NotRedefinedMethodError.
-	def self.process_stream(in_io, out_io)
-		raise NotRedefinedMethodError
-	end
+  # Process a file.
+  # An argument 'in_io' indicates an io (file handle) for input.
+  # Another argument 'out_io' indicates an io (file handle) for output.
+  # This method must be redefined in a subclass or be overridden.
+  # If not redefined, raise an exception TextFilter::NotRedefinedMethodError.
+  def self.process_stream(in_io, out_io)
+    raise NotRedefinedMethodError
+  end
 
 end
 
--- test/test_tefil.rb
+++ test/test_tefil.rb
@@ -7,141 +7,141 @@ require "tempfile"
 require "fileutils"
 
 module TextFilter
-	def self.process_stream(in_file, out_file)
-		results = []
-		in_file.each do |line|
-			out_file.puts line.sub('a', 'A')
-		end
-	end
+  def self.process_stream(in_file, out_file)
+    results = []
+    in_file.each do |line|
+      out_file.puts line.sub('a', 'A')
+    end
+  end
 end
 
 class TestTefil < Test::Unit::TestCase
-	TMP00 = "test/tmp00"
-	TMP01 = "test/tmp01"
+  TMP00 = "test/tmp00"
+  TMP01 = "test/tmp01"
 
-	def setup
-		FileUtils.rm TMP00 if File.exist? TMP00
-		FileUtils.rm TMP01 if File.exist? TMP01
-		File.open(TMP00, "w") do |io|
-			io.puts "abc"
-			io.puts "def"
-		end
-		File.open(TMP01, "w") do |io|
-			io.puts "abc"
-			io.puts "def"
-			io.puts "cab"
-		end
-	end
+  def setup
+    FileUtils.rm TMP00 if File.exist? TMP00
+    FileUtils.rm TMP01 if File.exist? TMP01
+    File.open(TMP00, "w") do |io|
+      io.puts "abc"
+      io.puts "def"
+    end
+    File.open(TMP01, "w") do |io|
+      io.puts "abc"
+      io.puts "def"
+      io.puts "cab"
+    end
+  end
 
-	def teardown
-		FileUtils.rm TMP00 if File.exist? TMP00
-		FileUtils.rm TMP01 if File.exist? TMP01
-	end
+  def teardown
+    FileUtils.rm TMP00 if File.exist? TMP00
+    FileUtils.rm TMP01 if File.exist? TMP01
+  end
 
-	def test_self_run
-		# Not found
-		assert_raise(Errno::ENOENT){ TextFilter.run([""]) }
-		assert_raise(Errno::ENOENT){ TextFilter.run([""], true) }
+  def test_self_run
+    # Not found
+    assert_raise(Errno::ENOENT){ TextFilter.run([""]) }
+    assert_raise(Errno::ENOENT){ TextFilter.run([""], true) }
 
-		# ファイル指定なしで標準入力
-		$stdin = StringIO.new
-		$stdin.puts "abc"
-		$stdin.puts "def"
-		$stdin.rewind
-		# stdout
-		$stdout = StringIO.new
-		TextFilter.run([], false)
-		$stdout.rewind
-		t = $stdout.readlines
-		assert_equal([ "Abc\n", "def\n" ], t)
-		$stdout.close
+    # ファイル指定なしで標準入力
+    $stdin = StringIO.new
+    $stdin.puts "abc"
+    $stdin.puts "def"
+    $stdin.rewind
+    # stdout
+    $stdout = StringIO.new
+    TextFilter.run([], false)
+    $stdout.rewind
+    t = $stdout.readlines
+    assert_equal([ "Abc\n", "def\n" ], t)
+    $stdout.close
 
-		$stdin = StringIO.new
-		$stdin.puts "abc"
-		$stdin.puts "def"
-		$stdin.rewind
-		$stdout = StringIO.new
-		TextFilter.run([], true)
-		$stdout.rewind
-		t = $stdout.readlines
-		assert_equal([ "Abc\n", "def\n" ], t)
-		$stdout.close
+    $stdin = StringIO.new
+    $stdin.puts "abc"
+    $stdin.puts "def"
+    $stdin.rewind
+    $stdout = StringIO.new
+    TextFilter.run([], true)
+    $stdout.rewind
+    t = $stdout.readlines
+    assert_equal([ "Abc\n", "def\n" ], t)
+    $stdout.close
 
-		# 単数のファイルを指定。
-		# overwrite なし。
-		setup
-		$stdout = StringIO.new
-		TextFilter.run([TMP00])
-		$stdout.rewind
-		tmp = $stdout.readlines
-		assert_equal(["Abc\n", "def\n"], tmp)
-		$stdout.close
-		tmp = File.open(TMP00, "r").readlines
-		assert_equal(["abc\n", "def\n"], tmp)
-		tmp = File.open(TMP01, "r").readlines
-		assert_equal(["abc\n", "def\n", "cab\n"], tmp)
+    # 単数のファイルを指定。
+    # overwrite なし。
+    setup
+    $stdout = StringIO.new
+    TextFilter.run([TMP00])
+    $stdout.rewind
+    tmp = $stdout.readlines
+    assert_equal(["Abc\n", "def\n"], tmp)
+    $stdout.close
+    tmp = File.open(TMP00, "r").readlines
+    assert_equal(["abc\n", "def\n"], tmp)
+    tmp = File.open(TMP01, "r").readlines
+    assert_equal(["abc\n", "def\n", "cab\n"], tmp)
 
-		# overwrite あり
-		setup
-		$stdout = StringIO.new
-		TextFilter.run([TMP00], true)
-		$stdout.rewind
-		tmp = $stdout.readlines
-		assert_equal([], tmp)
-		$stdout.close
-		tmp = File.open(TMP00, "r").readlines
-		assert_equal(["Abc\n", "def\n"], tmp)
-		tmp = File.open(TMP01, "r").readlines
-		assert_equal(["abc\n", "def\n", "cab\n"], tmp)
+    # overwrite あり
+    setup
+    $stdout = StringIO.new
+    TextFilter.run([TMP00], true)
+    $stdout.rewind
+    tmp = $stdout.readlines
+    assert_equal([], tmp)
+    $stdout.close
+    tmp = File.open(TMP00, "r").readlines
+    assert_equal(["Abc\n", "def\n"], tmp)
+    tmp = File.open(TMP01, "r").readlines
+    assert_equal(["abc\n", "def\n", "cab\n"], tmp)
 
-		# 複数のファイルを指定。
-		# overwrite なし。
-		setup
-		$stdout = StringIO.new
-		TextFilter.run([TMP00, TMP01])
-		$stdout.rewind
-		tmp = $stdout.readlines
-		assert_equal(["Abc\n", "def\n", "Abc\n", "def\n", "cAb\n"], tmp)
-		$stdout.close
-		tmp = File.open(TMP00, "r").readlines
-		assert_equal(["abc\n", "def\n"], tmp)
-		tmp = File.open(TMP01, "r").readlines
-		assert_equal(["abc\n", "def\n", "cab\n"], tmp)
+    # 複数のファイルを指定。
+    # overwrite なし。
+    setup
+    $stdout = StringIO.new
+    TextFilter.run([TMP00, TMP01])
+    $stdout.rewind
+    tmp = $stdout.readlines
+    assert_equal(["Abc\n", "def\n", "Abc\n", "def\n", "cAb\n"], tmp)
+    $stdout.close
+    tmp = File.open(TMP00, "r").readlines
+    assert_equal(["abc\n", "def\n"], tmp)
+    tmp = File.open(TMP01, "r").readlines
+    assert_equal(["abc\n", "def\n", "cab\n"], tmp)
 
-		# overwrite あり。
-		setup
-		$stdout = StringIO.new
-		TextFilter.run([TMP00, TMP01], true)
-		$stdout.rewind
-		stdout = $stdout.readlines
-		assert_equal([], stdout)
-		$stdout.close
-		tmp = File.open(TMP00, "r").readlines
-		assert_equal(["Abc\n", "def\n"], tmp)
-		tmp = File.open(TMP01, "r").readlines
-		assert_equal(["Abc\n", "def\n", "cAb\n"], tmp)
+    # overwrite あり。
+    setup
+    $stdout = StringIO.new
+    TextFilter.run([TMP00, TMP01], true)
+    $stdout.rewind
+    stdout = $stdout.readlines
+    assert_equal([], stdout)
+    $stdout.close
+    tmp = File.open(TMP00, "r").readlines
+    assert_equal(["Abc\n", "def\n"], tmp)
+    tmp = File.open(TMP01, "r").readlines
+    assert_equal(["Abc\n", "def\n", "cAb\n"], tmp)
 
-		# グローバル変数の標準出力、標準入力を元に戻す。
-		$stdout = STDOUT
-		$stdin  = STDIN
-	end
+    # グローバル変数の標準出力、標準入力を元に戻す。
+    $stdout = STDOUT
+    $stdin  = STDIN
+  end
 
-	def test_textfilter_command
-		#result = `echo "ab" | textfilter a A`
-		#assert_equal("Ab\n", result)
+  def test_textfilter_command
+    #result = `echo "ab" | textfilter a A`
+    #assert_equal("Ab\n", result)
 
-		#setup
-		#result = `textfilter a A #{TMP00}`
-		#assert_equal("Abc\ndef\n", result)
-		#str = File.open(TMP00, "r").read
-		#assert_equal("abc\ndef\n", str)
+    #setup
+    #result = `textfilter a A #{TMP00}`
+    #assert_equal("Abc\ndef\n", result)
+    #str = File.open(TMP00, "r").read
+    #assert_equal("abc\ndef\n", str)
 
-		#setup
-		#result = `textfilter -o a A #{TMP00}`
-		#assert_equal("", result)
-		#str = File.open(TMP00, "r").read
-		#assert_equal("Abc\ndef\n", str)
-	end
+    #setup
+    #result = `textfilter -o a A #{TMP00}`
+    #assert_equal("", result)
+    #str = File.open(TMP00, "r").read
+    #assert_equal("Abc\ndef\n", str)
+  end
 
 end
 
