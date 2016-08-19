6a015c9c33520a0bf3772f779cd6856f03bc176d
ippei94da <ippei94da@gmail.com>
2016-08-19 16:42:03 +0900

    daily change

---
+++ lib/tefil/statistics.rb
@@ -0,0 +1,35 @@
+class Tefil::Statistics < Tefil::TextFilterBase
+
+  HISTGRAM_LIMIT = 50
+
+  def initialize(options = {})
+    options[:smart_filename] = true
+    @minimum = options[:minimum]
+    super(options)
+  end
+
+  def process_stream(in_io, out_io)
+    data = in_io.readlines.map{|l| l.to_f}
+
+    sum = 0.0
+    data.each { |datum| sum += datum }
+
+    average = sum / data.size.to_f
+
+    #variance
+    tmp = 0.0
+    data.each { |datum| tmp += (datum - average)**2 }
+    variance = tmp / data.size.to_f
+
+    #standard deviation
+    standard_deviation = Math::sqrt(variance)
+
+    out_io.puts "sum:                #{sum}"
+    out_io.puts "average:            #{average}"
+    out_io.puts "variance:           #{variance}"
+    out_io.puts "standard_deviation: #{standard_deviation}"
+  end
+
+end
+
+
---
+++ test/test_statistics.rb
@@ -0,0 +1,35 @@
+#! /usr/bin/env ruby
+# coding: utf-8
+
+require "helper"
+require "stringio"
+
+#class Tefil::IndentConverter
+#  public :process_stream
+#end
+
+class TC_Statistics < Test::Unit::TestCase
+  def setup
+    @s00 = Tefil::Statistics.new()
+  end
+
+  def test_process_stream
+    # stdin -> stdout
+    $stdin = StringIO.new
+    $stdin.puts "1.0"
+    $stdin.puts "2.0"
+    $stdin.puts "3.0"
+    $stdin.rewind
+    #str = capture_stdout{}
+    result = capture_stdout{ @s00.filter([])}
+    correct =
+      "sum:                6.0\n" +
+      "average:            2.0\n" +
+      "variance:           0.6666666666666666\n" +
+      "standard_deviation: 0.816496580927726\n"
+    assert_equal(correct, result)
+  end
+end
+
+
+
